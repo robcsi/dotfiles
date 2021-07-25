2ba7f432282dfc498ceca7bf3268a2194213d8ea
Robert Sarkozi <sarkozi.robert@gmail.com>
2021-07-25 14:21:53 +0300

    Fixed sumneko_lua path

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -882,7 +882,7 @@ else
 end
 
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_root_path = vim.fn.stdpath("cache") .. "~/git/lua-language-server"
+local sumneko_root_path = vim.fn.stdpath("cache") .. "/lua-language-server"
 local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
 -- local sumneko_root_path = "/bin"
 -- local sumneko_binary = sumneko_root_path .. "/lua-language-server"
