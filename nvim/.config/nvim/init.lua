75c8bd5998396c8c58deaa14644897a26724d199
Robert Sarkozi <sarkozi.robert@gmail.com>
2022-07-11 13:49:49 +0300

    Added shortcut for switching between C++ header and source

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1022,3 +1022,6 @@ vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr
 vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", {silent = true, noremap = true})
+
+-- switch between header and source in C++
+vim.api.nvim_set_keymap("n", "<M-h>", "<cmd>ClangdSwitchSourceHeader<cr>", {silent = true, noremap = true})
