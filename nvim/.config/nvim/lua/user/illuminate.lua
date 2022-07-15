a7a52bd5f53e8daabfad3c2646f57a0217ba6bcf
Robert Sarkozi <sarkozi.robert@gmail.com>
2022-07-15 23:41:51 +0300

    Remapped illuminate

--- nvim/.config/nvim/lua/user/illuminate.lua
+++ nvim/.config/nvim/lua/user/illuminate.lua
@@ -1,4 +1,4 @@
 vim.g.Illuminate_ftblacklist = {'startify', 'NvimTree'}
-vim.api.nvim_set_keymap('n', '<a-n>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<a-p>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<a-j>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<a-k>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
 
