b3993ed6f5c9a1a342cd561bc35fbe92855a1048
Robert Sarkozi <sarkozi.robert@gmail.com>
2022-07-19 15:51:09 +0300

    Some Telescope settings changes

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -8,60 +8,59 @@ telescope.load_extension("project")
 
 telescope.setup {
   defaults = {
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case"
-    },
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-    file_ignore_patterns = { ".git/", "node_modules" },
-    entry_prefix = "  ",
-    initial_mode = "insert",
-    selection_strategy = "reset",
-    sorting_strategy = "ascending",
-    layout_strategy = "horizontal",
-    layout_config = {
-      horizontal = {
-        mirror = false
-      },
-      vertical = {
-        mirror = false
-      },
-      width = 0.75,
-      prompt_position = "top",
-      preview_cutoff = 120
-    },
-    preview_height = 1,
-    preview_width = 0.8,
-    file_sorter = require "telescope.sorters".get_fuzzy_file,
-    generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
-    -- path_display = true,
-    winblend = 0,
-    border = {},
-    borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
-    color_devicons = true,
-    use_less = true,
-    set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
-    file_previewer = require "telescope.previewers".vim_buffer_cat.new,
-    grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
-    qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker,
-    mappings = {
-      i = {
-        ["<C-q"] = actions.send_to_qflist,
-        ["<Down>"] = actions.cycle_history_next,
-        ["<Up>"] = actions.cycle_history_prev,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-      },
-    }
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case"
+        },
+        prompt_prefix = " ",
+        selection_caret = " ",
+        path_display = { "smart" },
+        file_ignore_patterns = { ".git/", "node_modules", ".cache" },
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                mirror = false
+            },
+            vertical = {
+                mirror = false
+            },
+            height = 0.8,
+            width = 0.8,
+            prompt_position = "top",
+            preview_cutoff = 120,
+            preview_width = 0.55,
+            preview_height = 0.5,
+        },
+        file_sorter = require "telescope.sorters".get_fuzzy_file,
+        generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
+        winblend = 0,
+        border = true,
+        borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+        color_devicons = true,
+        set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
+        file_previewer = require "telescope.previewers".vim_buffer_cat.new,
+        grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
+        qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker,
+        mappings = {
+            i = {
+                ["<C-q"] = actions.send_to_qflist,
+                ["<Down>"] = actions.cycle_history_next,
+                ["<Up>"] = actions.cycle_history_prev,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+            },
+        }
   },
   pickers = {
     -- Your special builtin config goes in here
