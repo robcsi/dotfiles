89f4c82ce5fda56664df0dc1170e707f5589b90a
Robert Sarkozi <sarkozi.robert@gmail.com>
2022-07-21 21:57:01 +0300

    Removed custom_areas from bufferline

--- nvim/.config/nvim/lua/user/bufferline.lua
+++ nvim/.config/nvim/lua/user/bufferline.lua
@@ -9,33 +9,33 @@ bufferline.setup {
         right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
         offsets = { { filetype = "NvimTree", text = "File Explorer", padding = 1 } },
         separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-        custom_areas = {
-            right = function()
-                local result = {}
-                local seve = vim.diagnostic.severity
-                local error = #vim.diagnostic.get(0, {severity = seve.ERROR})
-                local warning = #vim.diagnostic.get(0, {severity = seve.WARN})
-                local info = #vim.diagnostic.get(0, {severity = seve.INFO})
-                local hint = #vim.diagnostic.get(0, {severity = seve.HINT})
-
-                if error ~= 0 then
-                    table.insert(result, {text = "  " .. error, guifg = "#EC5241"})
-                end
-
-                if warning ~= 0 then
-                    table.insert(result, {text = "  " .. warning, guifg = "#EFB839"})
-                end
-
-                if hint ~= 0 then
-                    table.insert(result, {text = "  " .. hint, guifg = "#A3BA5E"})
-                end
-
-                if info ~= 0 then
-                    table.insert(result, {text = "  " .. info, guifg = "#7EA9A7"})
-                end
-                return result
-            end,
-        }
+        -- custom_areas = {
+        --     right = function()
+        --         local result = {}
+        --         local seve = vim.diagnostic.severity
+        --         local error = #vim.diagnostic.get(0, {severity = seve.ERROR})
+        --         local warning = #vim.diagnostic.get(0, {severity = seve.WARN})
+        --         local info = #vim.diagnostic.get(0, {severity = seve.INFO})
+        --         local hint = #vim.diagnostic.get(0, {severity = seve.HINT})
+        --
+        --         if error ~= 0 then
+        --             table.insert(result, {text = "  " .. error, guifg = "#EC5241"})
+        --         end
+        --
+        --         if warning ~= 0 then
+        --             table.insert(result, {text = "  " .. warning, guifg = "#EFB839"})
+        --         end
+        --
+        --         if hint ~= 0 then
+        --             table.insert(result, {text = "  " .. hint, guifg = "#A3BA5E"})
+        --         end
+        --
+        --         if info ~= 0 then
+        --             table.insert(result, {text = "  " .. info, guifg = "#7EA9A7"})
+        --         end
+        --         return result
+        --     end,
+        -- }
 
     },
     highlights = {
