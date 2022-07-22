0a73ffb39f0d476a604f6ac2383f11b6d6d1fa82
Robert Sarkozi <sarkozi.robert@gmail.com>
2022-07-22 11:24:36 +0300

    Removed trailing space

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -297,7 +297,7 @@ keymap("n", "<M-i>", "<cmd>BrowseInputSearch<cr>", opts)
 -- bufferline
 keymap("n", "<leader>bp", ":BufferLinePick<cr>", opts)
 
---focus 
+--focus
 keymap('n', '<M-l>', ':FocusSplitNicely<CR>', opts)
 keymap('n', '<leader>h', ':FocusSplitLeft<CR>', opts)
 keymap('n', '<leader>j', ':FocusSplitDown<CR>', opts)
