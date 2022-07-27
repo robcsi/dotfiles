95a2885df952448be757ab1a6c83003512dd7b80
Robert Sarkozi <sarkozi.robert@gmail.com>
2022-07-27 11:54:40 +0300

    Focus plugin settings regarding window focus colors

--- nvim/.config/nvim/lua/user/focus.lua
+++ nvim/.config/nvim/lua/user/focus.lua
@@ -5,13 +5,16 @@ end
 
 --settings here: https://github.com/beauwilliams/focus.nvim
 focus.setup {
-    winhighlight = false,
-    hybridnumber = false
+    autoresize = false,
+    winhighlight = true,
+    hybridnumber = false,
+    signcolumn = auto,
+    relativenumber = false
 }
 
 -- By default, the highlight groups are setup as such:
 --   hi default link FocusedWindow VertSplit
 --   hi default link UnfocusedWindow Normal
 -- To change them, you can link them to a different highlight group, see `:h hi-default` for more info.
-vim.cmd('hi link UnfocusedWindow CursorLine')
-vim.cmd('hi link FocusedWindow VisualNOS')
+vim.cmd('hi link UnfocusedWindow Normal')
+vim.cmd('hi link FocusedWindow CursorLine')
