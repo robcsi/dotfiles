a5c58f7919efe6538df20513bb3faca4f18b1085
Robert Sarkozi <sarkozi.robert@gmail.com>
2022-12-18 17:14:50 +0200

    Better Startify settings

--- nvim/.config/nvim/lua/user/startify.lua
+++ nvim/.config/nvim/lua/user/startify.lua
@@ -1,6 +1,7 @@
-local status_ok, startify = pcall(require, "startify")
-if not status_ok then
-  return
-end
+-- writing old-style vim-script commands, because Startify doesn't seem to have a lua interface?
+-- it cannot be loaded life other plugins, with pcall
 
-vim.g.startify_session_persistence = 1
+vim.cmd [[
+    let g:startify_session_persistence = 1
+    let g:startify_files_number = 20
+]]
