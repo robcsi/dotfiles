a5adcc43ff2baf62cf565958b8b707b102ea6b44
Robert Sarkozi <sarkozi.robert@gmail.com>
2023-04-17 10:26:20 +0300

    Fix dap-ui issue

--- nvim/.config/nvim/lua/plugins/dap-ui.lua
+++ nvim/.config/nvim/lua/plugins/dap-ui.lua
@@ -2,17 +2,6 @@ return {
     "rcarriga/nvim-dap-ui",
     requires = { "mfussenegger/nvim-dap" },
     config = function()
-        require("dapui").setup({
-local dap, dapui = require("dap"), require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
-        })
+        require("dapui").setup()
     end,
 }
