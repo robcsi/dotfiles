de38cb17453edcc5f166645cd18d4d94d3caf378
Robert Sarkozi <sarkozi.robert@gmail.com>
2023-10-04 16:42:24 +0300

    Fixed null-ls name change

--- nvim/.config/nvim/lua/plugins/borders.lua
+++ nvim/.config/nvim/lua/plugins/borders.lua
@@ -4,57 +4,57 @@
 local BORDER_STYLE = "rounded"
 
 return {
-  -- lazyvim.plugins.coding
-  {
-    "nvim-cmp",
-    opts = function(_, opts)
-      local bordered = require("cmp.config.window").bordered
-      return vim.tbl_deep_extend("force", opts, {
-        window = {
-          completion = bordered(BORDER_STYLE),
-          documentation = bordered(BORDER_STYLE),
+    -- lazyvim.plugins.coding
+    {
+        "nvim-cmp",
+        opts = function(_, opts)
+            local bordered = require("cmp.config.window").bordered
+            return vim.tbl_deep_extend("force", opts, {
+                window = {
+                    completion = bordered(BORDER_STYLE),
+                    documentation = bordered(BORDER_STYLE),
+                },
+            })
+        end,
+    },
+
+    -- lazyvim.plugins.editor
+    {
+        "which-key.nvim",
+        opts = { window = { border = BORDER_STYLE } },
+    },
+    {
+        "gitsigns.nvim",
+        opts = { preview_config = { border = BORDER_STYLE } },
+    },
+
+    -- lazyvim.plugins.lsp
+    {
+        "nvim-lspconfig",
+        opts = function(_, opts)
+            -- Set LspInfo border
+            require("lspconfig.ui.windows").default_options.border = BORDER_STYLE
+            return opts
+        end,
+    },
+
+    {
+        "none-ls.nvim",
+        opts = { border = BORDER_STYLE },
+    },
+
+    {
+        "mason.nvim",
+        opts = {
+            ui = { border = BORDER_STYLE },
         },
-      })
-    end,
-  },
-
-  -- lazyvim.plugins.editor
-  {
-    "which-key.nvim",
-    opts = { window = { border = BORDER_STYLE } },
-  },
-  {
-    "gitsigns.nvim",
-    opts = { preview_config = { border = BORDER_STYLE } },
-  },
-
-  -- lazyvim.plugins.lsp
-  {
-    "nvim-lspconfig",
-    opts = function(_, opts)
-      -- Set LspInfo border
-      require("lspconfig.ui.windows").default_options.border = BORDER_STYLE
-      return opts
-    end,
-  },
-
-  {
-    "null-ls.nvim",
-    opts = { border = BORDER_STYLE },
-  },
-
-  {
-    "mason.nvim",
-    opts = {
-      ui = { border = BORDER_STYLE },
     },
-  },
 
-  -- lazyvim.plugins.ui
-  {
-    "noice.nvim",
-    opts = {
-      presets = { lsp_doc_border = true },
+    -- lazyvim.plugins.ui
+    {
+        "noice.nvim",
+        opts = {
+            presets = { lsp_doc_border = true },
+        },
     },
-  },
 }
